235c76a55c4ccc6d565cb1a16e6091228bda5979
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-20 23:21:31 +0600

    feat: Input field part added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// input field part
+document.getElementById('btn-search').addEventListener('click', function () {
+    const inputField = document.getElementById('input-field');
+    const inputFieldText = inputField.value;
+    console.log(inputFieldText);
+});
