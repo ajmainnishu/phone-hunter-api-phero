2b536dad8cc2b62f5f58561fad098540c7a12b1c
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-21 08:45:29 +0600

    feat: Code organize

--- js/common.js
+++ js/common.js
@@ -1,8 +1,29 @@
-// button
+// buttons
 const loadingButtonData = (dataLimit) => {
     spinner(true);
     const inputField = document.getElementById('input-field');
     const inputFieldText = inputField.value;
     loadingFetchData(inputFieldText, dataLimit);
-    // inputField.value = '';
+}
+
+// data found or not message
+const dataFoundMessage = (phones) => {
+    const notFoundMessage = document.getElementById('not-found-message');
+    if (phones.length === 0) {
+        notFoundMessage.classList.remove('d-none');
+    }
+    else {
+        notFoundMessage.classList.add('d-none');
+    }
+}
+
+// display spinner
+const spinner = isLoading => {
+    const spinnerLoader = document.getElementById('spinner');
+    if (isLoading) {
+        spinnerLoader.classList.remove('d-none');
+    }
+    else {
+        spinnerLoader.classList.add('d-none');
+    }
 }
